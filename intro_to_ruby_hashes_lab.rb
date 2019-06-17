6ee7984bf8e99ac45606b1356e7745a5235da350
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-06-17 19:46:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
-  id_generator = {:id => rand(100)}
+  id_generator = {:id => rand(100000)}
 end
 
 def my_hash_creator(key, value)
